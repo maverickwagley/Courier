1a60a8a2b7e3b1bfaf0fc72c1e104a7898f84387
Maverick Wagley <guildedibis@gmail.com>
2024-06-21 22:11:14 -0400

    Multiplayer nothing

--- Scripts/GameScripts/scr_mp_controller.gd
+++ Scripts/GameScripts/scr_mp_controller.gd
@@ -1,62 +1,60 @@
 extends Control
 
-
 @export var ip_address = "127.0.0.1"
-@export var port = 8910
+@export var port = 8080
 var peer = ENetMultiplayerPeer.new()
-
-# Called when the node enters the scene tree for the first time.
+#
+#Built-in Methods
+#
 func _ready():
 	multiplayer.peer_connected.connect(peer_connected)
 	multiplayer.peer_disconnected.connect(peer_disconnected)
 	multiplayer.connected_to_server.connect(connected_to_server)
 	multiplayer.connection_failed.connect(connection_failed)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+#
 func _process(delta):
 	pass
-
-
+#
+#Custom Methods
+#
 func peer_connected(id):
 	print("Player Connected:" + str(id))
-
-
+#
 func peer_disconnected(id):
 	print("Player Disconnected:" + str(id))
-
-
+#
 func connected_to_server():
 	print("Connected to Server!")
 	send_player_info.rpc_id(1,$LineEdit.text,multiplayer.get_unique_id())
-
-
+#
 func connection_failed():
 	print("Connection Failed")
-
-
+#
 func _on_host_button_down():
+	print_debug("Starting host!")
 	peer = ENetMultiplayerPeer.new()
-	var error = peer.create_server(port, 4)
-	if error != OK:
-		print("Cannot Host:" + error)
-		return
-	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
-	multiplayer.set_multiplayer_peer(peer)
-	print("Waiting for players...")
-	send_player_info($LineEdit.text, multiplayer.get_unique_id())
-
-
+	#old version
+	#peer = ENetMultiplayerPeer.new()
+	#var error = peer.create_server(port, 4)
+	#if error != OK:
+		#print("Cannot Host:" + error)
+		#return
+	#peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	#multiplayer.set_multiplayer_peer(peer)
+	#print("Waiting for players...")
+	#send_player_info($LineEdit.text, multiplayer.get_unique_id())
+#
 func _on_join_button_down():
 	peer = ENetMultiplayerPeer.new()
 	peer.create_client(ip_address, port)
 	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
 	multiplayer.set_multiplayer_peer(peer)
-
-
+#
 func _on_start_button_down():
 	start_game.rpc()
-
+#
+#RPC Methods
+#
 @rpc("any_peer")
 func send_player_info(name, id):
 	if !ScrGameManager.player.has(id):
@@ -68,8 +66,7 @@ func send_player_info(name, id):
 	if multiplayer.is_server():
 		for i in ScrGameManager.player:
 			send_player_info.rpc(ScrGameManager.player[i].name, i)
-			
-
+#
 @rpc("any_peer", "call_local")
 func start_game():
 	#var game_scene = load("res://Scenes/Room/Lenko/rm_world.tscn").instantiate()
